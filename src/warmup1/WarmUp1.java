33c83215a171982352bc4ad1f4a58a576dc44db9
Yvette Menase <yvette.j.menase@gmail.com>
2017-05-23 23:31:01 -0600

    last commit for the night

--- src/warmup1/WarmUp1.java
+++ src/warmup1/WarmUp1.java
@@ -7,9 +7,6 @@ public class WarmUp1 {
 	// true if we are on vacation. We sleep in if it is not a weekday or we're on vacation.
 	// Return true if we sleep in.
 	//
-	// sleepIn(false, false) → true
-	// sleepIn(true, false) → false
-	// sleepIn(false, true) → true
 
 	public boolean sleepIn(boolean weekday, boolean vacation) {
 	  return !weekday || vacation;
@@ -19,10 +16,6 @@ public class WarmUp1 {
 	// sumDouble
 	//
 	// Given two int values, return their sum. Unless the two values are the same, then return double their sum.
-	//
-	// sumDouble(1, 2) → 3
-	// sumDouble(3, 2) → 5
-	// sumDouble(2, 2) → 8
 	public int sumDouble(int a, int b) {
 	  int sum = a + b;
 
@@ -36,10 +29,6 @@ public class WarmUp1 {
 	// diff21
 	// Given an int n, return the absolute difference between n and 21, except
 	// return double the absolute difference if n is over 21.
-	//
-	// diff21(19) → 2
-	// diff21(10) → 11
-	// diff21(21) → 0
 
 	public int diff21(int n) {
 	  if (n <= 21){
@@ -56,19 +45,13 @@ public class WarmUp1 {
 	// the range 0..23. We are in trouble if the parrot is talking and the hour is before
 	// 7 or after 20. Return true if we are in trouble.
 	//
-	// parrotTrouble(true, 6) → true
-	// parrotTrouble(true, 7) → false
-	// parrotTrouble(false, 6) → false
 
 	public boolean parrotTrouble(boolean talking, int hour) {
 	  return (talking && (hour < 7 || hour > 20));
 	}
 
-	/* Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+	//Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
 
-	makes10(9, 10) → true
-	makes10(9, 9) → false
-	makes10(1, 9) → true */
 	public boolean makes10(int a, int b) {
 	  return (a == 10 || b == 10 || a+b == 10);
 	}
@@ -77,11 +60,6 @@ public class WarmUp1 {
 	// nearHundred
 	// Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num)
 	// computes the absolute value of a number.
-	//
-	// nearHundred(93) → true
-	// nearHundred(90) → true
-	// nearHundred(89) → false
-
 	public boolean nearHundred(int n) {
 	  return Math.abs(100-n) <= 10 || Math.abs(200 - n) <= 10;
 	}
--- src/warmup1/WarmUp1Test.java
+++ src/warmup1/WarmUp1Test.java
@@ -12,8 +12,24 @@ public class WarmUp1Test {
 		assertEquals(wu1.sleepIn(false, false), true);
 		assertEquals(wu1.sleepIn(true, false), false);
 		assertEquals(wu1.sleepIn(false, true), true);
+		assertEquals(wu1.sumDouble(1, 2), 3);
+		assertEquals(wu1.sumDouble(3, 2), 5);
+		assertEquals(wu1.sumDouble(2, 2), 8);
+		assertEquals(wu1.diff21(19), 2);
+		assertEquals(wu1.diff21(10), 11);
+		assertEquals(wu1.diff21(21), 0);
+		assertEquals(wu1.parrotTrouble(true, 6), true);
+		assertEquals(wu1.parrotTrouble(true, 7), false);
+		assertEquals(wu1.parrotTrouble(false, 6), false);
+		assertEquals(wu1.makes10(9, 10), true);
+		assertEquals(wu1.makes10(9, 9), false);
+		assertEquals(wu1.makes10(1, 9), true);
+		assertEquals(wu1.nearHundred(93), true);
+		assertEquals(wu1.nearHundred(90), true);
+		assertEquals(wu1.nearHundred(89), false);
 		
 		
+
 	}
 
 }
