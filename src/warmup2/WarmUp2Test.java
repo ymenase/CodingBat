25bac3de70eb08cfdcbf86888f37f1e1edebad26
Yvette Menase <yvette.j.menase@gmail.com>
2017-05-24 10:26:48 -0600

    adding more tests to warmup2

--- src/warmup2/WarmUp2Test.java
+++ src/warmup2/WarmUp2Test.java
@@ -14,6 +14,21 @@ public class WarmUp2Test {
 		assertEquals(warmUp2.frontTimes("Chocolate", 2), "ChoCho");
 		assertEquals(warmUp2.frontTimes("Chocolate", 3), "ChoChoCho");
 		assertEquals(warmUp2.frontTimes("Abc", 3), "AbcAbcAbc");
+		assertEquals(warmUp2.countXX("abcxx"), 1);
+		assertEquals(warmUp2.countXX("xxx"), 2);
+		assertEquals(warmUp2.countXX("xxxx"), 3);
+		assertEquals(warmUp2.doubleX("axxbb"), true);
+		assertEquals(warmUp2.doubleX("axaxax"), false);
+		assertEquals(warmUp2.doubleX("xxxxx"), true);
+		assertEquals(warmUp2.stringBits("Hello"), "Hlo");
+		assertEquals(warmUp2.stringBits("Bye"), "Be");
+		assertEquals(warmUp2.stringBits("Heeololeo"), "Hello");
+		assertEquals(warmUp2.stringSplosion("Code"), "CCoCodCode");
+		assertEquals(warmUp2.stringSplosion("abc"), "aababc");
+		assertEquals(warmUp2.stringSplosion("ab"), "aab");
+		assertEquals(warmUp2.last2("hixxhi"), 1);
+		assertEquals(warmUp2.last2("xaxxaxaxx"), 1);
+		assertEquals(warmUp2.last2("axxxaaxx"), 2);
 		
 	}
 }
