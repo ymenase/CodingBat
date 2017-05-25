95723e238e5eeb11f176a41481a9c70732c26a3b
Yvette Menase <yvette.j.menase@gmail.com>
2017-05-24 18:59:57 -0600

    revamped tests to make them more modularized -- logic2 and array1

--- src/array1/Array1Test.java
+++ src/array1/Array1Test.java
@@ -6,44 +6,82 @@ import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
 
 public class Array1Test {
+	Array1 array1 = new Array1();		
 
 	@Test
-	public void test() {
-		
-		Array1 array1 = new Array1();		
-		
+	public void firstLast6() {
 		assertEquals(array1.firstLast6(new int[] {1, 2, 6}), true);
 		assertEquals(array1.firstLast6(new int[] {6, 1, 2, 3}), true);
 		assertEquals(array1.firstLast6(new int[] {13, 6, 1, 2, 3}), false);
+	}
+	
+	@Test
+	public void sameFirstLast(){
 		assertEquals(array1.sameFirstLast(new int[] {1,2,3}), false);
 		assertEquals(array1.sameFirstLast(new int[] {1,2,3, 1}), true);
 		assertEquals(array1.sameFirstLast(new int[] {1,2,1}), true);
+	}
+	
+	@Test
+	public void makePi(){
 		assertArrayEquals(array1.makePi(), new int[] {3, 1, 4});
+	}
+	
+	@Test
+	public void commonEnd(){
 		assertEquals(array1.commonEnd(new int[] {1, 2, 3}, new int[] {7, 3}), true);
 		assertEquals(array1.commonEnd(new int[] {1, 2, 3}, new int[]{7, 3, 2}), false);
 		assertEquals(array1.commonEnd(new int[] {1, 2, 3}, new int[] {1, 3}), true);
+	}
+	
+	@Test
+	public void sum3(){
 		assertEquals(array1.sum3(new int[] {1, 2, 3}), 6);
 		assertEquals(array1.sum3(new int[] {5, 11, 2}), 18);
 		assertEquals(array1.sum3(new int[] {7, 0, 0}), 7);
+	}
+	
+	@Test
+	public void rotateLeft3(){
 		assertArrayEquals(array1.rotateLeft3(new int[] {1, 2, 3}), new int[] {2, 3, 1});
 		assertArrayEquals(array1.rotateLeft3(new int[] {5, 11, 9}), new int[] {11, 9, 5});
 		assertArrayEquals(array1.rotateLeft3(new int[] {7, 0, 0}), new int[] {0, 0, 7});
+	}
+	
+	@Test
+	public void reverse3(){
 		assertArrayEquals(array1.reverse3(new int[] {1, 2, 3}), new int[] {3, 2, 1});
 		assertArrayEquals(array1.reverse3(new int[] {5, 11, 9}), new int[] {9, 11, 5});
 		assertArrayEquals(array1.reverse3(new int[] {7, 0, 0}), new int[] {0, 0, 7});
+	}
+	
+	@Test
+	public void maxEnd3(){
 		assertArrayEquals(array1.maxEnd3(new int[] {1, 2, 3}), new int[] {3, 3, 3});
 		assertArrayEquals(array1.maxEnd3(new int[] {11, 5, 9}), new int[] {11, 11, 11});
 		assertArrayEquals(array1.maxEnd3(new int[] {2, 11, 3}), new int[] {3, 3, 3});
+	}
+	
+	@Test
+	public void makeLast(){
 		assertArrayEquals(array1.makeLast(new int[] {4, 5, 6}), new int[] {0, 0, 0, 0, 0, 6});
 		assertArrayEquals(array1.makeLast(new int[] {1, 3}), new int[] {0, 0, 0, 3});
 		assertArrayEquals(array1.makeLast(new int[] {4}), new int[] {0, 4});
+	}
+	
+	@Test
+	public void double23(){
 		assertEquals(array1.double23(new int[] {2, 2}), true);
 		assertEquals(array1.double23(new int[] {3, 3}), true);
 		assertEquals(array1.double23(new int[] {2, 3}), false);
+	}
+	
+	@Test
+	public void fix23(){
 		assertArrayEquals(array1.fix23(new int[] {1, 2, 3}), new int[] {1, 2, 0});
 		assertArrayEquals(array1.fix23(new int[] {2, 3, 5}), new int[] {2, 0, 5});
 		assertArrayEquals(array1.fix23(new int[] {1, 2, 1}), new int[] {1, 2, 1});
-		
-		
 	}
+	
+
 }
--- src/logic2/Logic2Test.java
+++ src/logic2/Logic2Test.java
@@ -5,25 +5,37 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class Logic2Test {
+	Logic2 logic2 = new Logic2();
+
 	@Test
-	public void test() {
-		Logic2 logic2 = new Logic2();
+	public void makeBricks() {
 		assertEquals(logic2.makeBricks(3, 1, 8), true);
 		assertEquals(logic2.makeBricks(3, 1, 10), false);
 		assertEquals(logic2.makeBricks(3, 4, 10), true);
+	}
+
+	public void loneSum() {
 		assertEquals(logic2.loneSum(1, 2, 3), 6);
 		assertEquals(logic2.loneSum(3, 2, 3), 2);
 		assertEquals(logic2.loneSum(3, 3, 3), 0);
+	}
+
+	public void luckySum() {
 		assertEquals(logic2.luckySum(1, 2, 3), 6);
 		assertEquals(logic2.luckySum(1, 2, 13), 3);
 		assertEquals(logic2.luckySum(1, 13, 3), 1);
+	}
+
+	public void noTeenSum() {
 		assertEquals(logic2.noTeenSum(1, 2, 3), 6);
 		assertEquals(logic2.noTeenSum(2, 13, 1), 3);
 		assertEquals(logic2.noTeenSum(2, 1, 14), 3);
+	}
+
+	public void closeFar() {
 		assertEquals(logic2.closeFar(1, 2, 10), true);
 		assertEquals(logic2.closeFar(1, 2, 3), false);
 		assertEquals(logic2.closeFar(4, 1, 3), true);
+	}
 
-		
-	}	
 }
