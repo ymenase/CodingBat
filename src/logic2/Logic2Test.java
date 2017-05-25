f44bf5e44dcb27f677861c2050a11e045cb7aea5
Yvette Menase <yvette.j.menase@gmail.com>
2017-05-24 19:05:41 -0600

    modularized WarmUp2 tests

--- src/logic2/Logic2Test.java
+++ src/logic2/Logic2Test.java
@@ -13,25 +13,29 @@ public class Logic2Test {
 		assertEquals(logic2.makeBricks(3, 1, 10), false);
 		assertEquals(logic2.makeBricks(3, 4, 10), true);
 	}
-
+	
+	@Test
 	public void loneSum() {
 		assertEquals(logic2.loneSum(1, 2, 3), 6);
 		assertEquals(logic2.loneSum(3, 2, 3), 2);
 		assertEquals(logic2.loneSum(3, 3, 3), 0);
 	}
 
+	@Test
 	public void luckySum() {
 		assertEquals(logic2.luckySum(1, 2, 3), 6);
 		assertEquals(logic2.luckySum(1, 2, 13), 3);
 		assertEquals(logic2.luckySum(1, 13, 3), 1);
 	}
 
+	@Test
 	public void noTeenSum() {
 		assertEquals(logic2.noTeenSum(1, 2, 3), 6);
 		assertEquals(logic2.noTeenSum(2, 13, 1), 3);
 		assertEquals(logic2.noTeenSum(2, 1, 14), 3);
 	}
-
+	
+	@Test
 	public void closeFar() {
 		assertEquals(logic2.closeFar(1, 2, 10), true);
 		assertEquals(logic2.closeFar(1, 2, 3), false);
--- src/warmup2/WarmUp2Test.java
+++ src/warmup2/WarmUp2Test.java
@@ -5,33 +5,60 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class WarmUp2Test {
+	WarmUp2 warmUp2 = new WarmUp2();
+
 	@Test
-	public void test() {
-		WarmUp2 warmUp2 = new WarmUp2();
+	public void stringTimes() {
 		assertEquals(warmUp2.stringTimes("Bear", 2), "BearBear");
 		assertEquals(warmUp2.stringTimes("Hi", 3), "HiHiHi");
 		assertEquals(warmUp2.stringTimes("Dog", 1), "Dog");
+	}
+
+	@Test
+	public void frontTimes() {
 		assertEquals(warmUp2.frontTimes("Chocolate", 2), "ChoCho");
 		assertEquals(warmUp2.frontTimes("Chocolate", 3), "ChoChoCho");
 		assertEquals(warmUp2.frontTimes("Abc", 3), "AbcAbcAbc");
+	}
+
+	@Test
+	public void countXX() {
 		assertEquals(warmUp2.countXX("abcxx"), 1);
 		assertEquals(warmUp2.countXX("xxx"), 2);
 		assertEquals(warmUp2.countXX("xxxx"), 3);
+	}
+
+	@Test
+	public void doubleX() {
 		assertEquals(warmUp2.doubleX("axxbb"), true);
 		assertEquals(warmUp2.doubleX("axaxax"), false);
 		assertEquals(warmUp2.doubleX("xxxxx"), true);
+	}
+
+	@Test
+	public void stringBits() {
 		assertEquals(warmUp2.stringBits("Hello"), "Hlo");
 		assertEquals(warmUp2.stringBits("Bye"), "Be");
 		assertEquals(warmUp2.stringBits("Heeololeo"), "Hello");
+	}
+
+	@Test
+	public void stringSplosion() {
 		assertEquals(warmUp2.stringSplosion("Code"), "CCoCodCode");
 		assertEquals(warmUp2.stringSplosion("abc"), "aababc");
 		assertEquals(warmUp2.stringSplosion("ab"), "aab");
+	}
+
+	@Test
+	public void last2() {
 		assertEquals(warmUp2.last2("hixxhi"), 1);
 		assertEquals(warmUp2.last2("xaxxaxaxx"), 1);
 		assertEquals(warmUp2.last2("axxxaaxx"), 2);
-		//assertEquals(warmUp2.arrayFront9(new int[] {1, 2, 9, 3, 4}), true);
-		
-
-		
 	}
+
+	@Test
+	public void arrayCount9() {
+		assertEquals(warmUp2.arrayCount9(new int[] { 1, 2, 9, 3, 4 }), 1);
+	}
+
 }
