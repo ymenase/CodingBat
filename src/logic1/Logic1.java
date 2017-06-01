084258b970d6f6d493d940a1afa9b0d4c66947af
Yvette Menase <yvette.j.menase@gmail.com>
2017-06-01 13:56:52 -0600

    working on more exercises, no new tests

--- src/logic1/Logic1.java
+++ src/logic1/Logic1.java
@@ -191,6 +191,147 @@ public class Logic1 {
 		  }
 		  return false;
 		}
+	
+//	Logic-1 > less20 
+//	Return true if the given non-negative number is 1 or 2 less than a multiple of 20. 
+	//So for example 38 and 39 return true, but 40 returns false.
+//
+//	less20(18) → true
+//	less20(19) → true
+//	less20(20) → false
+	
+	public boolean less20(int n) {
+		  if ((n + 1) % 20 == 0) {
+		    return true;
+		  }
+		  if ((n + 2) % 20 == 0) {
+		    return true;
+		  }
+		  return false;
+		}
+	
+	//nearTen 
+//	Given a non-negative number "num", return true if num is within 2 of a multiple of 
+//	10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. 
+//
+//	nearTen(12) → true
+//	nearTen(17) → false
+//	nearTen(19) → true
+
+	//teenSum
+
+	//Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky. 
+	//So if either value is a teen, just return 19.
+	//teenSum(3, 4) → 7
+	//teenSum(10, 13) → 19
+	//teenSum(13, 2) → 19
+	
+	public int teenSum(int a, int b) {
+		  int sum = a + b;
+		  if ((a >= 13 && a <= 19) || (b >=13 && b<=19)){
+		    return 19;
+		  }
+		  return sum;
+		}
+
+	//answerCell
+	//Your cell phone rings. Return true if you should answer it. 
+	//Normally you answer, except in the morning you only answer if it is your mom calling. 
+	//In all cases, if you are asleep, you do not answer.
+	//	answerCell(false, false, false) → true
+	//	answerCell(false, false, true) → false
+	//	answerCell(true, false, false) → false
+	
+	public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+		  if ((isMorning == true) && (isMom == true) && !isAsleep){
+		    return true;
+		  }
+		  if (!isAsleep && !isMorning){
+		    return true;
+		  }
+		 else return false;
+		}
+
+	//teaParty
+	//We are having a party with amounts of tea and candy. Return the int outcome of the party 
+	//encoded as 0=bad, 1=good, or 2=great. A party is good (1) if both tea and candy are at least 5. 
+	//However, if either tea or candy is at least double the amount of the other one, the party is great (2). However, in all cases, if either tea or candy is less than 5, the party is always bad (0).
+	//
+	//teaParty(6, 8) → 1
+	//teaParty(3, 8) → 0
+	//teaParty(20, 6) → 2
+	public int teaParty(int tea, int candy) {
+		  if (tea < 5 || candy < 5){
+		    return 0;
+		  }
+		  
+		  if (tea >= candy * 2 || candy >= tea * 2){
+		    return 2;
+		  }
+		  if (tea >= 5 && candy >=5){
+		    return 1;
+		  }
+		  
+		  return 0;
+		}
+	
+	//fizzString
+	//Given a string str, if the string starts with "f" return "Fizz". 
+	//If the string ends with "b" return "Buzz". If both the "f" and "b" 
+	//conditions are true, return "FizzBuzz". In all other cases, return the 
+	//string unchanged. 
+//	fizzString("fig") → "Fizz"
+//	fizzString("dib") → "Buzz"
+//	fizzString("fib") → "FizzBuzz"
+	public String fizzString(String str) {
+		  if (str.startsWith("f") && str.endsWith("b")){
+		    return "FizzBuzz";
+		  }
+		  if (str.startsWith("f")){
+		    return "Fizz";
+		  }
+		  if (str.endsWith("b")){
+		    return "Buzz";
+		  } 
+		  return str;
+		}
+	
+	//twoAsOne
+	//Given three ints, a b c, return true if it is possible to add two of the ints to 
+	//get the third.
+	//	twoAsOne(1, 2, 3) → true
+	//	twoAsOne(3, 1, 2) → true
+	//	twoAsOne(3, 2, 2) → false
+	
+	public boolean twoAsOne(int a, int b, int c) {
+		  if (a + b == c) {
+		    return true;
+		  }
+		  if (b + c == a){
+		    return true;
+		  }
+		  if (a + c == b){
+		    return true;
+		  }
+		  return false;
+		}
+	
+	//inOrder
+	//Given three ints, a b c, return true if b is greater than a, and c is greater than b. 
+	//However, with the exception that if "bOk" is true, b does not need to be greater than a.
+	//inOrder(1, 2, 4, false) → true
+	//inOrder(1, 2, 1, false) → false
+	//inOrder(1, 1, 2, true) → true
+	
+	public boolean inOrder(int a, int b, int c, boolean bOk) {
+		  if (bOk) {
+		    return c > b;
+		  }
+		  return b > a && c > b;
+		  
+		}
+
+	
 
 
 }
